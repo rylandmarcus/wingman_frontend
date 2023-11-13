23b5e6af016abdb6a7650bb833a8c7e7a2085693
ryland <ryland@MacBook-Pro-4.hawaii.rr.com>
2023-11-13 13:31:05 -0800

    time for socket

---
+++ src/components/Chatdisplay.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Chatdisplay = () => {
+  return (
+    <div>Chatdisplay</div>
+  )
+}
+
+export default Chatdisplay
--- src/components/Conversation.jsx
+++ src/components/Conversation.jsx
@@ -1,14 +1,34 @@
 import React, { useEffect, useState } from 'react'
+import Chatdisplay from './Chatdisplay'
 
 const Conversation = ({convo}) => {
-    // const chat = {...convo}
-    const [chat, setChat] = useState({...convo})
+    //socket time...good luck haahha ok i think to start join a room and just send an unsaved message back and forth to other people
+    const token = localStorage.getItem('token')
+    const [chat, setChat] = useState(convo)
+    console.log(chat);
     useEffect(()=>{
-        setChat({...convo})
-        console.log(convo);
+        async function fetchConversation (){
+            const response = await fetch(process.env.REACT_APP_URL+'chats/'+token+'/chat/'+convo._id);
+            const data = await response.json()
+            setChat({...data})
+            console.log(chat.users[0]);
+        }
+        fetchConversation()
+        // setChat({...convo})
+        // console.log(convo);
+        // return ()=>{
+        //     setChat({})
+        //     console.log('unmounting');
+        // }
     },[convo])
     return (
-        <div>There is a Conversation opne with the id of: {chat._id}</div>
+        <div>
+            <div>Conversation with {chat.users[0].firstName}</div>
+            {chat.messages.length==0 ? <div>Start the conversation!</div> : <Chatdisplay></Chatdisplay>}
+            <input type="text" />
+            <button>Send</button>
+        </div>
+        
     )
 }
 
